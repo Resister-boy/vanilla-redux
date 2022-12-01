84fc46f51b899c92540c4d08af073167fa000fe6
Resister-boy <hassanpumped17@gmail.com>
2022-12-01 23:41:58 +0900

    redux state tutorial in vanilla js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createStore } from "redux";
 
-import App from './App';
+const add = document.querySelector("#add");
+const minus = document.querySelector("#minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+const ADD = "ADD";
+const MINUS = "MINUS"; 
+
+// A Function that a modifing our data;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return (count + 1);
+    case MINUS:
+      return (count - 1);
+    default: 
+      return (count); // return data;
+  }
+};
+
+// A Function that a store our data;
+const countStore = createStore(countModifier);
+/*
+1. dispatch()
+2. subscribe()
+3. getState()
+4. replaceReducer()
+*/
+
+countStore.dispatch({ // dispatch: sending message to modifier;
+  type: ADD
+});
+countStore.dispatch({
+  type: MINUS
+});
+
+const checkIsChange = () => {
+  number.innerText = countStore.getState(); // getState: get data from store;;
+}
+
+countStore.subscribe(checkIsChange) // subscribe: tracking data that we are changing;
+
+const handleAdd = () => {
+  countStore.dispatch({
+    type: ADD
+  });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({
+    type: MINUS
+  });
+};
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+add.addEventListener("click", handleAdd);
+
+minus.addEventListener("click", handleMinus);
